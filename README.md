f7d74cf175c5d83cf5d8596617f0f339ee26ada3
RetzudosServitor <christoph@nnpro.at>
2016-09-08 14:10:40 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+django-emailuser
+================
+
+This package implements a custom Django user model and manager that replaces the
+default user model with one that *only* has an e-mail field, not username *and*
+e-mail.
+
+It's basically a copy&paste of Django 1.10 code with the necessary (small)
+adjustments.
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-emailuser',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Custom user model with no username and only email',
+    long_description='Custom user model with no username and only email',
+    url='https://github.com/Retzudo/django-emailuser',
+    author='Retzudo',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
